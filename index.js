90aae7ce0b56971a5b8552fb7e9efe0a5000fd69
BHAVDIP BHUKAN <128157200+bhukanbhavdip@users.noreply.github.com>
2024-05-01 13:32:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get("/",(req,res)=>{
+    res.setHeader("Access-Controls-Access-Credentials","true")
     return res.status(200).send({message:"welcome to ecommerce api - node"})
 })
 
